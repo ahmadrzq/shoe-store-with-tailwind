4d88e89b3ec0ef4d55f5cc70ac3d89ef95dc3e5c
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-02-06 15:59:45 +0700

    Customize color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
       },
+      colors: {
+        primary1: "#003e29",
+        primary2: "#e4bba1",
+        primary3: "#d9d9d9",
+        borderColor: "#787878",
+        bgColor: "#ededed",
+        bgGreen: "#023423",
+      }
     },
   },
   plugins: [],
